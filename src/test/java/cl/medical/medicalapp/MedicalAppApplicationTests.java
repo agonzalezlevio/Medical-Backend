52318bf2e66564c12913a2bb71d4affeb7062504
agonzalezlevio <agonzalezlevio.dev@gmail.com>
2022-01-29 17:25:47 -0300

    first commit

---
+++ src/test/java/cl/medical/medicalapp/MedicalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.medical.medicalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
