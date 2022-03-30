c932035eb439587125ed5d19bdc2a8e0bfc3206d
agonzalezlevio <agonzalezlevio.dev@gmail.com>
2022-03-29 22:24:23 -0300

    chore(controller): add tag annotation in doctor controller

--- src/main/java/cl/medical/medicalapp/controller/DoctorController.java
+++ src/main/java/cl/medical/medicalapp/controller/DoctorController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("doctor")
+@Tag(name = "doctor")
 public class DoctorController {
 
     private final IDoctorService doctorService;
